b538b07253823f03ccdde62e3d3945d79564d94a
yurim <milooy@icloud.com>
2013-08-09 01:52:35 +0900

    5. King클래스 getPossibleMoves 구현 (KingTest주석 수정)

--- test/pieces/KingTest.java
+++ test/pieces/KingTest.java
@@ -11,7 +11,7 @@ public class KingTest extends TestCase {
 		System.out.println(king.getPossibleMoves());
 		assertEquals(8, king.getPossibleMoves().size());
 
-		Position source2 = new Position("h1");		//(3,4)위치 
+		Position source2 = new Position("h1");		//(7,0)위치.구석이라 이동가능한 위치가 3개뿐이다. 
 		Piece king2 = new King(Color.WHITE, source2);
 		
 		System.out.println(king2.getPossibleMoves());
