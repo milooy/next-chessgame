4e1ef3b994cc6ee8eb3c8d02843d3ea38af794e1
yurim <milooy@icloud.com>
2013-08-09 17:35:54 +0900

    7.5 . Bishop클래스 대폭 수정. PositionController의 존재를 모르고 엄청난 뻘짓을 했었다는 것을 깨달음. 전에 1시간 걸려 짠 더러운 코드가 1분만에 짜짐. 멘붕.

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -12,49 +12,11 @@ public class Bishop extends Piece {
 	@Override
 	public List<Position> getPossibleMoves() {
 		List<Position> possibleMoves = new ArrayList<Position>();
-		
-		//position의 y값에서 x값을 빼준 값 minusGapNum을 구한다. 
-		int minusGapNum = super.position.getX() - super.position.getY();
-		//position의 y값에서 x값을 더해준 값 plusGapNum을 구한다.
-		int plusGapNum = super.position.getY() + super.position.getX();
-	
-		//current position의 x좌표를 구해 2개 만들어준다. 각각 돌리는데 사용한다.
-		int curX1 = super.position.getX();
-		int curX2 = super.position.getX();
-		
-		//임시 포지션 
-		Position testPosition;
-		
-		//좌표가 유효하지 않은 것이 나올때까지 무한루프 
-		while(true){
-			curX1--;
-			curX2++;
-			
-			//왼쪽 위 대각선 
-			testPosition = new Position(curX1, plusGapNum-curX1);
-			if(testPosition.isValid())
-				possibleMoves.add(testPosition);
-				
-			//왼쪽 아래 대각선 
-			testPosition = new Position(curX1, curX1-minusGapNum);
-			if(testPosition.isValid())
-				possibleMoves.add(testPosition);
-			
-			//오른쪽 아래 대각선 
-			testPosition = new Position(curX2, plusGapNum-curX2);
-			if(testPosition.isValid())
-				possibleMoves.add(testPosition);
-			
-			//오른쪽 위 대각선 
-			testPosition = new Position(curX2, curX2-minusGapNum);
-			if(testPosition.isValid())
-				possibleMoves.add(testPosition);
-			
-			//네 방향 다 구한 후 유효하지 않은 좌표가 나오면 while을 빠져나간다.
-			if(!testPosition.isValid())
-				break;
-		}
 
+		PositionController posC = new PositionController(super.position);
+		for(int i=0; i<posC.findsDiagonalPositionAll().size(); i++){
+			possibleMoves.add(posC.findsDiagonalPositionAll().get(i));
+		}
 		return possibleMoves;
 	}
 }
